18e43ab73920bb47246352754f61c2d8df417d69
pupil <pupil@ivkhk.ee>
2023-09-14 14:04:58 +0300

    first commit

---
+++ src/jptv22myfirstpro/App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptv22myfirstpro;
+
+/**
+ *
+ * @author Sanya
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        App app = new App();
+        System.out.print("app.toString = "+app.toString());
+    }
+    
+}
--- src/jptv22myfirstpro/Myfirst.java
+++ src/jptv22myfirstpro/Myfirst.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jktv22myfirstpro.newpackage;
+package jptv22myfirstpro;
 
 /**
  *
  * @author pupil
  */
-public class NewMain {
+public class Myfirst {
 
     /**
      * @param args the command line arguments
