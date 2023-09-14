d3c765b01e94ed73ae828eb4d4e23ece1103dee3
pupil <pupil@ivkhk.ee>
2023-09-14 13:12:29 +0300

    first commit

--- src/jktv22myfirstpro/NewMain.java
+++ src/jktv22myfirstpro/NewMain.java
@@ -3,14 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tasks;
+package jktv22myfirstpro.newpackage;
 
 /**
  *
  * @author pupil
  */
-public class MyStrings {
-    public void doStrings(){
-        System.out.println("Разберем, что такое строки");
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
+    
 }
